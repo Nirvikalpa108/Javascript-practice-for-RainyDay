5b3912e49b09616c22a5026c18cfedcce0e69b97
Amina <amina.adewusi@gmail.com>
2018-11-11 09:41:59 +0000

    Random number connected to IF statement
    The random number now connects to the IF statement so that when the button is clicked and a temperature is shown, we are given clothing advice.

--- script.js
+++ script.js
@@ -3,13 +3,12 @@
 function generateRandomNumber() {
     let rand = Math.floor((Math.random() * 35) + (-10));
     document.getElementById("demo").innerHTML = rand;
-}
 
-if (temp < 14) {
+if (rand < 14) {
     document.getElementById("advice").innerHTML = "wear a jacket";
-} else if (temp < 25) {
+} else if (rand < 25) {
     document.getElementById("advice").innerHTML = "wear a t-shirt";
 } else {
     document.getElementById("advice").innerHTML = "wear a vest";
 }
-
+}
