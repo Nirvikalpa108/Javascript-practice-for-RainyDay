ef3f8f732304f5290b09564688a5bbf0e78d6bcb
Nirvikalpa108 <amina.adewusi@gmail.com>
2018-11-18 05:41:48 +0000

    XMLHttpRequest prepare new API
    Decided not to use Dark Sky weather API after speaking to Andra Georgescu, because of the need to use a proxy server which is complex for my level of ability. Here I am preparing the code to access a new API. Probably from Open weather.

--- script.js
+++ script.js
@@ -1,43 +1,33 @@
-// Create a request variable and assign a new XMLHttpRequest object to it.
-let request = new XMLHttpRequest();
-
-// Open a new connection, using the GET request on the URL endpoint
-// QUESTION - how do I hide my darksky key? 
-// QUESTION - have I excluded the data blocks correctly?
-request.open('GET', 'ENTER DARK SKY KEY'/[51.461040],[0.138160]/[minutely,hourly,daily,alerts,flags]);
-
-request.onload = function () {
-  // Onload, means upon receiving response. Add console.log - as soon as some data comes back, I get console log.
-  //  nothing will happen until i get the data back.
+function loadXMLDoc() {
+    let request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        document.getElementById("demo").innerHTML =
+        this.responseText;
+      }
+    };
+    request.open("GET", "API goes here", true);
+    // Onload, means upon receiving response. As soon as some data comes back, I get console log.
+    request.onload = function () {
+      console.log("API send request working")
+      }
+    // request.send is sending the request to the server, so I could do this after button click
+    request.send();
   }
 
-// Send request to server. Make when I click button.
-request.send();
-
-// Begin accessing JSON data here
+  //not sure if I still need this? Begin accessing JSON data here
 let data = JSON.parse(this.response);
-
-data.forEach(temperature => {
-  console.log(temperature);
-  // QUESTION I want to console.log Dark Sky's temperature key. Correct?
-});
-
-// Some ideas from https://github.com/deanbot/dark-sky-api
-
-DarkSkyApi.loadCurrent()
-  .then(result => console.log(result));
-
 
 //Amina's random number generator and IF statements for temperature
 function generateRandomNumber() {
-    let rand = Math.floor((Math.random() * 35) + (-10));
-    document.getElementById("demo").innerHTML = rand;
+  let rand = Math.floor((Math.random() * 35) + (-10));
+  document.getElementById("demo").innerHTML = rand;
 
 if (rand < 14) {
-    document.getElementById("advice").innerHTML = "Jacket";
+  document.getElementById("advice").innerHTML = "Jacket";
 } else if (rand < 25) {
-    document.getElementById("advice").innerHTML = "T-shirt";
+  document.getElementById("advice").innerHTML = "T-shirt";
 } else {
-    document.getElementById("advice").innerHTML = "Vest";
+  document.getElementById("advice").innerHTML = "Vest";
 }
 }
