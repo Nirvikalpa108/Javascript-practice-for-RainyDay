7b0b37ab4c8f7c511fb704a57693a8e130ec6c63
Nirvikalpa108 <amina.adewusi@gmail.com>
2018-11-19 18:43:01 +0000

    added JSON.parse to get temp parameter

--- script.js
+++ script.js
@@ -2,28 +2,25 @@ function loadXMLDoc() {
     let request = new XMLHttpRequest();
     request.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("demo").innerHTML =
-        this.responseText;
+        let myObj = JSON.parse(this.responseText);
+          document.getElementById("demo").innerHTML =
+          myObj.main.temp;
       }
     };
-    request.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London&units=metric,uk&APPID=b626cf4076aabf3d744469a9ed262f07", true);
+    request.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b626cf4076aabf3d744469a9ed262f07&units=metric", true);
     // Console log at onload means I know as soon as some data comes back.
     request.onload = function () {
       console.log("API send request working")
     }
     request.send();
   }
-
-//Amina's random number generator and IF statements for temperature
-function generateRandomNumber() {
-  let rand = Math.floor((Math.random() * 35) + (-10));
-  document.getElementById("demo").innerHTML = rand;
-
-if (rand < 14) {
+  
+//not sure how to define the Javascript object response so that I can do IF statement
+/*if (??? < 14) {
   document.getElementById("advice").innerHTML = "Jacket";
-} else if (rand < 25) {
+} else if (??? < 25) {
   document.getElementById("advice").innerHTML = "T-shirt";
 } else {
   document.getElementById("advice").innerHTML = "Vest";
 }
-}
+*/
