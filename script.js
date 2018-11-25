18fc6b1972b1c3ad22713c230677455b5466a560
Nirvikalpa108 <amina.adewusi@gmail.com>
2018-11-25 17:09:36 +0000

    Import API key from config.js
    Trying to, unsuccessfully, import the API key from the config.js file.

--- script.js
+++ script.js
@@ -1,30 +1,29 @@
 function loadXMLDoc() {
-    let request = new XMLHttpRequest();
-    request.onreadystatechange = function() {
-      if (this.readyState == 4 && this.status == 200) {
-        var myObj = JSON.parse(this.responseText);
-          document.getElementById("temperature").innerHTML =
-          myObj.main.temp;
-          document.getElementById("wind").innerHTML =
-          myObj.wind.speed;
+var request = new XMLHttpRequest();
+request.onreadystatechange = function() {
+  if (this.readyState == 4 && this.status == 200) {
+  var myObj = JSON.parse(this.responseText);
+  document.getElementById("temperature").innerHTML = myObj.main.temp;
+  document.getElementById("wind").innerHTML = myObj.wind.speed;
 
-          if (myObj.main.temp > 25 && myObj.wind.speed < 10) {
-            document.getElementById("advice").innerHTML = "T-shirt";
-          } else if (myObj.main.temp < 25 > 15 && myObj.wind.speed < 10) {
-            document.getElementById("advice").innerHTML = "Jersey"; 
-          } else if (!(myObj.main.temp <= 14) && myObj.wind.speed < 24 > 11) {
-            document.getElementById("advice").innerHTML = "Jersey";
-          } else {
-            document.getElementById("advice").innerHTML = "Jacket";
-          }
-      }
-    };
-    request.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=b626cf4076aabf3d744469a9ed262f07&units=metric", true);
-    // Console log at onload means I know as soon as some data comes back.
+    if (myObj.main.temp > 25 && myObj.wind.speed < 10) {
+    document.getElementById("advice").innerHTML = "T-shirt";
+    } else if (myObj.main.temp < 25 > 15 && myObj.wind.speed < 10) {
+    document.getElementById("advice").innerHTML = "Jersey"; 
+    } else if (!(myObj.main.temp <= 14) && myObj.wind.speed < 24 > 11) {
+    document.getElementById("advice").innerHTML = "Jersey";
+    } else {
+    document.getElementById("advice").innerHTML = "Jacket";
+    }
+  }
+};
+    //getting ERROR unexpected identifier
+    import {YOUR_API_KEY} from "config.js";
+    console.log (YOUR_API_KEY)
+
+    request.open("GET", "YOUR_API_KEY", true);
     request.onload = function () {
       console.log("API send request working")
     }
     request.send();
   }
-  
-
