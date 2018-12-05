b4053c869e6b8097b0a4bdff2413498545be5000
Nirvikalpa108 <amina.adewusi@gmail.com>
2018-12-05 22:27:55 +0000

    corrected function

--- scripts/main.js
+++ scripts/main.js
@@ -1,47 +1,31 @@
 requirejs(["./config"], function(config) {
+
+
+  function getAdvice(temp, wind) {
+  var myAdvice = document.getElementById("advice")
+    if (temp > 26 && wind < 10) {
+    myAdvice.innerHTML="T-shirt";
+    } else if (15 < temp < 25 && wind < 10) {
+      myAdvice.innerHTML = "Jersey"; 
+    } else if (!(temp <= 14) && 11 < wind < 24) {
+      myAdvice.innerHTML = "Jersey";
+    } else {
+      myAdvice.innerHTML = "Jacket";
+    }
+    }
+
   function loadXMLDoc() {
     var request = new XMLHttpRequest();
     request.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
       var myObj = JSON.parse(this.responseText);
-      document.getElementById("temperature").innerHTML = myObj.main.temp;
-      document.getElementById("wind").innerHTML = myObj.wind.speed;
-
-//Mario's feedback
-
-      var temp = getTemperature(myObj.main.temp) {
-        // function returns constants 'COLD', 'MILD', 'HOT'
-      }
-      
-      var wind = getWindiness(myObj.main.wind) {
-        // function return constants e.g. 'STILL', 'MILD', 'WINDY'
-      }
-
-      switch (true) {
-      case (temp === 'HOT' && wind === 'STILL'):
-      document.getElementById("advice").innerHTML = "T-shirt";
-      break;
-      case (temp === 'MILD' && wind === 'STILL'):
-        ...
-      }
-```
- // switch statement I had before   
-
-      switch (myObj.main.temp, myObj.wind.speed) {
-        case myObj.main.temp > 25 && myObj.wind.speed < 10:
-        document.getElementById("advice").innerHTML = "T-shirt";
-        break;
-        case myObj.main.temp < 25 > 15 && myObj.wind.speed < 10:
-        document.getElementById("advice").innerHTML = "Jersey";
-        break;
-        case (!(myObj.main.temp <= 14) && myObj.wind.speed < 24 > 11):
-        document.getElementById("advice").innerHTML = "Jersey";
-        break;
-        default:
-        document.getElementById("advice").innerHTML = "Jacket";
-      }
-      }
-    };
+      var temp = myObj.main.temp;
+      var wind = myObj.wind.speed;
+      document.getElementById("temperature").innerHTML = temp;
+      document.getElementById("wind").innerHTML = wind;
+      getAdvice(temp, wind);
+      }  
+    }
         request.open("GET", config.YOUR_API_KEY, true);
         request.onload = function () {
           console.log("API send request working")
